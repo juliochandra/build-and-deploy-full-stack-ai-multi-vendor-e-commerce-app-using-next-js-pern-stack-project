46cd2907507ba6e39426807f1b899b77e8364b8b
juliochandra <juliochandra@gmail.com>
2026-05-19 18:46:35 +0700

    feat: setup inngest for clerk user webhooks

---
+++ inngest/functions.ts
@@ -0,0 +1,56 @@
+import { prisma } from "@/lib/prisma";
+import { inngest } from "./client";
+
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-create",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  },
+);
+
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: "sync-user-update",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  },
+);
+
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: "sync-user-delete",
+    triggers: [{ event: "clerk/user.delete" }],
+  },
+
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.delete({
+      where: { id: data.id },
+    });
+  },
+);
